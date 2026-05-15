c354fcfa37e7de78932a2ae79ad659b4173e6f1b
mas4112 <mas4112@gmail.com>
2026-05-16 02:57:26 +0600

    Add RemovePackages prebuilt to exclude packages
    Add a RemovePackages prebuilt module and include it in PRODUCT_PACKAGES to prevent specific apps from being built.
    
    Signed-off-by: mas4112 <mas4112@gmail.com>

--- device.mk
+++ device.mk
@@ -33,6 +33,10 @@ PRODUCT_PACKAGES += \
     SettingsResDevice \
     SystemUIResDevice
 
+# Exclude unwanted packages from building
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_kalama/audio_policy_configuration.xml \
